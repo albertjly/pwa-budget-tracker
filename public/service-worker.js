8d71afe1b00a795ab98727d3097ed5ccb2a7b852
Albert Jiang <albertjly@gmail.com>
2021-06-24 22:27:42 -0700

    change cache file paths

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,23 @@
 // Uncomment the lines below to set up the cache files
 //
-const CACHE_NAME = 'my-site-cache-v2';
-const DATA_CACHE_NAME = 'data-cache-v2';
+const CACHE_NAME = 'my-site-cache-budgetTracker';
+const DATA_CACHE_NAME = 'data-cache-budgetTracker';
 
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/assets/js/index.js',
-  '/assets/css/style.css',
-  '/assets/images/icons/icon-72x72.png',
-  '/assets/images/icons/icon-96x96.png',
-  '/assets/images/icons/icon-128x128.png',
-  '/assets/images/icons/icon-144x144.png',
-  '/assets/images/icons/icon-152x152.png',
-  '/assets/images/icons/icon-192x192.png',
-  '/assets/images/icons/icon-384x384.png',
-  '/assets/images/icons/icon-512x512.png'
+  '/js/index.js',
+  '/js/db.js',
+  '/css/style.css',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
 ];
 
 // Install the service worker
@@ -29,29 +30,8 @@ self.addEventListener('install', function (evt) {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
-
-  self.skipWaiting();
 });
 
-// Activate the service worker and remove old data from the cache
-// YOUR CODE HERE
-//
-self.addEventListener('active', function (evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log('Removing old cache data', key);
-            return cache.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
 // Intercept fetch requests
 // YOUR CODE HERE
 //
